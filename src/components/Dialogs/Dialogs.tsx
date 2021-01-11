c884a9b68d7501706b6ea77d61222e15c8587543
DimaK0489 <dim040489@gmail.com>
2021-01-11 15:12:30 +0300

    init commit

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import s from "./Dialogs.module.css"
 import {NavLink} from "react-router-dom";
 
+type DialogsItemPropsType={
+    id: number,
+    name: string
+}
 
-function DialogItem(props:any){
+type MessagesPropsType={
+    message: string
+}
+
+function DialogItem(props:DialogsItemPropsType){
     return(
         <div className={s.dialog + " " + s.active}>
             <NavLink to={"/dialogs/1" + props.id}>{props.name}</NavLink>
@@ -11,7 +19,8 @@ function DialogItem(props:any){
     );
 }
 
-function Message(props:any){
+
+function Message(props:MessagesPropsType){
     return(
         <div className={s.dialog}>{props.message}</div>
     );
