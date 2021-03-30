0f2313584d8f04c0ba7c482f93c5c640f4714860
Dima_Kaleinik <dim040489@gmail.com>
2021-03-30 16:30:10 +0300

    Rest API Follow Unfollow

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,6 +3,7 @@ import style from './users.module.css'
 import {UsersType} from "../../redux/users-reducer";
 import userPhoto from "../../assets/Image/user.png"
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 export type UsersPropsType = {
     unfollow: (userId: string) => void
@@ -26,7 +27,7 @@ let Users = (props: UsersPropsType) => {
         }
 
         return <div>
-            <div>
+            <div className={style.cursor}>
                 {pages.map(p => {
                     // @ts-ignore
                     return <span className={props.currentPage === p && style.selectedPage}
@@ -46,10 +47,32 @@ let Users = (props: UsersPropsType) => {
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0//follow/${u.id}`,  {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "bf0875ba-8463-481a-87a8-643832194416"
+                                    }
+                                })
+                                    .then(response => {
+                                        if( response.data.resultCode === 0) {
+                                            props.unfollow(u.id)
+                                        }
+                                    });
+
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                axios.post(`https://social-network.samuraijs.com/api/1.0//follow/${u.id}`, {}, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "bf0875ba-8463-481a-87a8-643832194416"
+                                    }
+                                })
+                                    .then(response => {
+                                        if( response.data.resultCode === 0) {
+                                            props.follow(u.id)
+                                        }
+                                    });
+
                             }}>Follow</button>
                         }
                     </div>
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -23,7 +23,10 @@ type UsersPropsType = {
 export class UsersContainer extends React.Component<UsersPropsType> {
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+            {
+                withCredentials: true
+            })
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
@@ -34,7 +37,10 @@ export class UsersContainer extends React.Component<UsersPropsType> {
     onPageChanged = (pageNumber: number) => {
         this.props.setCurrentPage(pageNumber);
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+            {
+                withCredentials: true
+            })
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
